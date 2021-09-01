cc92bbbccbecabdce1edec051715fbd08252b372
Arjun.Avagadda <arjunavagadda207@gmail.com>
2021-09-01 17:32:14 +0530

    Conditional statements updated

--- Condstatemnts.py
+++ Condstatemnts.py
@@ -1,1 +1,45 @@
+if True:
+    pass    # pass statement is used as placeholder.
 
+lang = 'Java'
+
+if lang == 'Python':
+    print(lang)
+else:
+    print('some other lang..')
+
+if lang == 'Python':
+    print(lang)
+elif lang == 'Java':
+    print('java lang')
+else:
+    print('not lang..')
+
+
+# and or not
+
+user = 'admin'
+logged_in = True
+
+if user == 'admin' and logged_in: ## and - both should be True, OR --> one should be True
+    print('get in')
+else:
+    print('Invalid')
+
+## is - 2 objets with same id..
+
+a = [1.2]
+b = a
+
+print(a is b) # True 
+
+"""
+
+::Evalutes to false::
+
+False None 
+() ,  '' , []
+{}
+
+
+"""
