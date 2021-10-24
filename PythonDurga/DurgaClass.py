acb8087fe0aa796bce08c6d48bf024477dc6bbec
arjun <arjunavagadda207@gmail.com>
2021-10-24 07:03:52 +0530

    Durgalecture added

---
+++ PythonDurga/DurgaClass.py
@@ -0,0 +1,88 @@
+print('arjun'+'20')
+
+print(3*'arj')
+
+print(ord('A'))
+
+print(chr(65))
+
+print('arjun' >= 'Arjun')
+
+print(True <= 1)
+
+# print(10<'arjun') # error
+
+print(10 < 20 < 30 < 40)
+print(10 < 20 < 30 < 40 > 50)  # False
+
+print(10 == 'ar')  # False # content comparison
+
+print(10 is 20)  # reference comparasion
+
+"""
+for non boolean type
+
+x and y
+
+if x evaluates true then result is y
+if x evaluates falsethen result is x
+
+"""
+
+print(10 and 20)  # 20
+
+print('' and 'arj')  # ''
+
+print(10 or 20)  # 10
+
+print(not 'arj')  # False
+
+print(++10)  # not increment operators 10
+
+
+print(--10)  # 10
+
+
+"""
+-- no increment and decrement operator
+
+"""
+
+print(20+30j)
+
+print(30 if 20 > 10 else 40)  # ternary operator
+
+# operator precedence
+
+# () ** *      /,%.// - all has same pref  + -
+
+
+"""
+module - consists of classes var and function
+if we have 2 mod python takes the recent one (no eroor)
+
+if we have 2 fucn new one given priority
+
+
+- math module
+
+dir(math)
+
+module alias - as keyword
+
+from math import sqrt
+from math import sqrt as s,pi as p
+
+"""
+
+# how to read input and 
+
+
+
+
+
+
+
+
+
+
